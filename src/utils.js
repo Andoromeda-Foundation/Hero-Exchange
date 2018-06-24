058619775eda3d21ce211ce6c4da4167fbe224bb
Frank Wei <frank@frankwei.xyz>
2018-06-24 18:27:24 +0800

    feat: UnitConversionTool

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { BigNumber } from "bignumber.js";
+
 export const getMe = async () => new Promise((resolve) => {
     window.postMessage({
       target: 'contentscript',
@@ -16,3 +18,15 @@ export const getHerosCardImages = (id) => ({
     front: `http://test.cdn.hackx.org/heros_new/${id}.jpeg`,
     back: `http://test.cdn.hackx.org/backs_new/${id}.jpeg`
 })
+
+export class UnitConversionTool {
+    static fromNasToWei(value) {
+      return new BigNumber('1000000000000000000').times(value);
+    }
+    static fromWeiToNas(value) {
+      if (value instanceof BigNumber) {
+        return value.dividedBy('1000000000000000000');
+      }
+      return new BigNumber(value).dividedBy('1000000000000000000');
+    }
+  }
