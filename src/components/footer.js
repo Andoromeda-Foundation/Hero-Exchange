444c2449c8721506b3d819542c513c22dd76ee5a
Frank Wei <frank@frankwei.xyz>
2018-06-24 16:53:27 +0800

    docs: update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-const MetaMaskDownloadURL = "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
+const DownloadURL = "https://chrome.google.com/webstore/detail/nasextwallet/gehjkhmhclgnkkhpfamakecfgakkfkco"
 export default function Footer() {
     return (<footer class="footer">
     <div class="content has-text-centered">
@@ -11,7 +11,7 @@ export default function Footer() {
         星云水浒™️ 是 仙女座科技有限公司的商标。
         <br />
         从 Google Chrome 应用商店下载 
-        <a href={MetaMaskDownloadURL}> MetaMask</a>, 一个开源且安全的以太坊钱包
+        <a href={DownloadURL}> NasExtWallet</a>, 一个开源且安全的星云链钱包
       </p>
     </div>
   </footer>)
